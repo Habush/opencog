3727b2db325d3ed6fd726b4c6fccfaeacd1541e6
Abdulrahman Semrie <hsamireh@gmail.com>
2017-11-27 12:20:32 +0300

    Made local modifications to Python REST Api

--- opencog/python/web/api/apischeme.py
+++ opencog/python/web/api/apischeme.py
@@ -1,8 +1,8 @@
 __author__ = 'Cosmo Harrigan'
 
-from flask import abort, jsonify
-from flask.ext.restful import Resource, reqparse
-from opencog.scheme_wrapper import scheme_eval, __init__
+from flask import abort, jsonify, request
+from flask_restful import Resource, reqparse
+from opencog.scheme_wrapper import scheme_eval, __init__, scheme_eval_as
 from flask_restful_swagger import swagger
 
 COGSERVER_PORT = 17001
@@ -27,7 +27,7 @@ class SchemeAPI(Resource):
         super(SchemeAPI, self).__init__()
 
     @swagger.operation(
-	notes='''
+    notes='''
 Include a JSON object with the POST request containing the command
 in a field named "command"
 
@@ -53,32 +53,46 @@ blocks until the request has finished.
 
 <p>This functionality is implemented as a POST method because it can
 cause side-effects.''',
-	responseClass='response',
-	nickname='post',
-	parameters=[
-	    {
-		'name': 'command',
-		'description': 'Scheme command',
-		'required': True,
-		'allowMultiple': False,
-		'dataType': 'string',
-		'paramType': 'body'
-	    }
-	],
-	responseMessages=[
-	    {'code': 200, 'message': 'Scheme command executed successfully'},
-	    {'code': 400, 'message': 'Invalid request: Required parameter command missing'}
-	]
+    responseClass='response',
+    nickname='post',
+    parameters=[
+        {
+        'name': 'command',
+        'description': 'Scheme command',
+        'required': True,
+        'allowMultiple': False,
+        'dataType': 'string',
+        'paramType': 'body'
+        }
+    ],
+    responseMessages=[
+        {'code': 200, 'message': 'Scheme command executed successfully'},
+        {'code': 400, 'message': 'Invalid request: Required parameter command missing'}
+    ]
     )
-    def post(self):
+    def options(self):
+        if request.method == 'OPTIONS':
+            headers = {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+                'Access-Control-Max-Age': 1000,
+                'Access-Control-Allow-Headers': 'origin, x-csrftoken, content-type, accept',
+            }
+            return 'success', 200, headers
+    def post (self):
         """
         Send a command to the Scheme interpreter
         """
 
         # Validate, parse and send the command
         data = reqparse.request.get_json()
+        # data = {u'command' : u'(cog-bind (findAboutGene (GeneNode \"IGLV1-50\")))'}
+        print data
         if 'command' in data:
+            print "data"
             response = scheme_eval(self.atomspace, data['command'])
+            response = scheme_parsor.parse(str(response), None, 1, None)
+            print "data22"
         else:
             abort(400,
                   'Invalid request: required parameter command is missing')
